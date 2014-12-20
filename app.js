8c2591127bace20a95e0fbde3779747b2c2bb35f
noname01 <hjc.noname@gmail.com>
2014-12-19 19:05:02 -0800

    country table and code refactor

--- app.js
+++ app.js
@@ -2,26 +2,35 @@ var myApp = angular.module("myApp", []);
 
 myApp.controller("NameCtrl", function($scope){
     var count = 0;
-
     $scope.name = "noname" + count;
-    $scope.names = ["foo", "bar", "foobar"];
-
-    $scope.addName = function(){
-        // angular does not allow duplicates in ng-repeat
-        if($scope.enteredName && $scope.names.indexOf($scope.enteredName) < 0)
-            $scope.names.push($scope.enteredName);
-        $scope.enteredName = "";
-    };
-
-    $scope.removeName = function(name){
-        var p = $scope.names.indexOf(name);
-        if(p > -1)
-            $scope.names.splice(p, 1);
-    };
 
     setInterval(function(){
         count++;
         $scope.name = "noname" + count;
         $scope.$apply();
     }, 1000);
+});
+
+myApp.controller("TodoCtrl", function($scope){
+    $scope.todos = ["foo", "bar", "foobar"];
+
+    $scope.addTodo = function(){
+        // angular does not allow duplicates in ng-repeat
+        if($scope.enteredTodo && $scope.todos.indexOf($scope.enteredTodo) < 0)
+            $scope.todos.push($scope.enteredTodo);
+        $scope.enteredTodo = "";
+    };
+
+    $scope.removeTodo = function(item){
+        var p = $scope.todos.indexOf(item);
+        if(p > -1)
+            $scope.todos.splice(p, 1);
+    };
+});
+
+myApp.controller("CountryCtrl", function($scope, $http){
+    $http.get("countries.json")
+        .success(function(data){
+            $scope.countries = data;
+        });
 });
